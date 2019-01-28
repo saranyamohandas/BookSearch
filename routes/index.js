87519c7e9e2bd25b276c06465579b5e6618eec70
Mayja Tom <maja.morales17@gmail.com>
2019-01-28 11:51:19 -0800

    having issues with button links on nav, errors with nodemon

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const savedPage = require("../client/src/pages/savedBooks");
 
 // API Routes
 router.use("/api", apiRoutes);
@@ -9,5 +10,9 @@ router.use("/api", apiRoutes);
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+//saved page route
+router.use("/seach");
+  res.sendFile(path.join(__dirname, savedPage));
 
 module.exports = router;
