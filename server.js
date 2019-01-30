a79bf1464b89ac8b5ab284ccb4db1d26af0cebbe
Mayja Tom <maja.morales17@gmail.com>
2019-01-30 10:31:15 -0800

    updating server.js with mongolab information

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+require("dotenv").config();
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -16,7 +16,13 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooksDB");
+// var MONGODB_URI = mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooksDB");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Start the API server
 app.listen(PORT, function() {
