994f85979d3c072ad0678a430a6684a3528d3f33
Mayja Tom <maja.morales17@gmail.com>
2019-01-29 20:19:51 -0800

    deployment to heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooksDB");
+mongoose.connect(process.env.MONGODB_URI || "mongolab-regular-25804");
 
 // Start the API server
 app.listen(PORT, function() {
